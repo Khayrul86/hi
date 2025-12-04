4d4a13fea4bfb6d274ef3c94d95164e3f5643b07
guy named person <69010022+Khayrul86@users.noreply.github.com>
2025-12-04 17:17:47 -0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,9 +1,9 @@
 -- hi the source code is available to everybody because yes
 if rconsoleinput then
   local inp = rconsoleinput()
-  if inp == "hi" then --default key
-    rconsoleprint("coolio")
+  if inp == "hi" then
+    print("cool")
   else
-    rconsoleprint("nah, id return false")
+    print("no")
   end
 end
