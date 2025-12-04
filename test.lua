46b7b4567005f160b35bbaab8109c57ac9c66f7b
guy named person <69010022+Khayrul86@users.noreply.github.com>
2025-12-04 17:13:00 -0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -2,16 +2,8 @@
 if rconsoleinput then
   local inp = rconsoleinput()
   if inp == "hi" then --default key
-    rprint("coolio")
+    rconsoleprint("coolio")
   else
-    print("nah, id return false")
+    rconsoleprint("nah, id return false")
   end
-else
-  error("didnt get rconsoleinput func")
-  local h = Instance.new("Hint",game.Players.LocalPlayer.PlayerGui)
-  h.Text = "hi this would probably show up if ur using a mobile exec or something that doesnt support rconsole funcs"
-  wait(2)
-  h.Text = "also pls only use this script for like making other scripts"
-  wait(1.5)
-  h:Destroy()
 end
